8fa1eb597146fbe7dfa0f5fef9d89335fa59bd59
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-07-03 09:13:35 -0300

    feat: adicionado cors liberando acesso ao frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { searchVideos } from './controllers/search-videos-controller';
 import { getFavorites, getTotalFavorites, insertFavorite, removeFavorite } from './controllers/favorites-controller';
 
 dotenv.config();
 export const app = express();
+app.use(cors())
 app.use(express.json())
 
 const port = process.env.PORT
